a86f4e1ecac9b2992410adc8ae855147dbec66ce
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2024-10-05 15:37:45 +0100

    add a source file to 10-pointers-applications

--- 10-pointers-applications/params-pointers.c
+++ 10-pointers-applications/params-pointers.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-int multiply_and_divide(int n1, int n2, int *product, int *quotient) {
+int multiply_and_divide(int n1, int n2, int *product, int *division) {
 
     /* Can't divide if n2 is 0 */
     if(n2 == 0)
         return -1;
 
     *product = n1 * n2;
-    *quotient = n1 / n2;
+    *division = n1 / n2;
 
     return 0;
 }
 
 int main(int argc, char **argv) {
     int a = 10, b = 5;
-    int product, quotient;
+    int product, division;
 
-    if(multiply_and_divide(a, b, &product, &quotient) == 0) {
+    if(multiply_and_divide(a, b, &product, &division) == 0) {
         printf("10*5 = %d\n", product);
-        printf("10/5 = %d\n", quotient);
+        printf("10/5 = %d\n", division);
     } else
         printf("ERROR: division by zero!\n");
 
